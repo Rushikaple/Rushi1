edf57efa462709c8e21f9c38c191255555d93cfa
Rushikesh Kaple <118681100+Rushikaple@users.noreply.github.com>
2022-12-02 10:47:39 +0000

    Add files via upload

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/foodieinc-user-app'),
+      dir: require('path').join(__dirname, './coverage/admin-dashboard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
